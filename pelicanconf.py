789af5045af793e02fc8a612e90417fcb1e914a1
offdutysysadmin <jonmoo@offdutysysadmin.com>
2015-11-27 14:52:12 -0600

    remove links

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,14 +20,11 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = ()
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Github', 'https://github.com/offdutypirate'),
+        ('Twitter', 'https://twitter.com/offdutypirate'),)
 
 DEFAULT_PAGINATION = 5
 

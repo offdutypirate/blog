cc1e11e29b4c72148d59e9c46a288ef9cc3a5b6f
offdutysysadmin <jonmoo@offdutysysadmin.com>
2015-11-27 15:54:36 -0600

    add CNAME for github pages

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,3 +30,5 @@ DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}

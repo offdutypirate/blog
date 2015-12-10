e9ac51df2728fdf614790bbc922609eb5b828b1c
offdutysysadmin <jonmoo@offdutysysadmin.com>
2015-12-10 11:23:58 -0600

    change http to https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://offdutypirate.com'
+SITEURL = 'https://offdutypirate.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
